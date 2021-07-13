e94b40b2049d474bd92c19e9998036e55880abed
Yuri Marcon <yuri.erik.oliveira@gmail.com>
2021-07-13 15:34:35 -0300

    Upload do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,67 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(props){
+  return (
+    <Box >
+      <img src={`https://github.com/${props.gitHubUser}.png`}></img>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+ const gitHubUser = "yurimarcon"
+ const pessoasFavoritas = [
+  "juunegreiros",
+  "omariosouto",
+  "peas"
+ ];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+          <ProfileSidebar gitHubUser={gitHubUser} />
+        </div>
+
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
+          <Box >
+            <h2 className="title">
+              Bem vindo(a)
+            </h2>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea'}}>
+        <ProfileRelationsBoxWrapper >
+          <h2 className="smallTitle">
+            Comunidades ({pessoasFavoritas.length})
+          </h2>
+          <ul>
+            {pessoasFavoritas.map((itemAtual)=>{
+              return(
+                <li>
+                  <a href={"#"} key={itemAtual}>
+                    <img src={`https://github.com/${itemAtual}.png`}></img>
+                    <span>{itemAtual}</span>
+                  </a>
+                </li>
+
+              )
+
+            })
+            }
+          </ul>
+        </ProfileRelationsBoxWrapper>
+        <Box >
+          Amigos(as)
+        </Box>
+      </div>
+      </MainGrid>
+    </>
+  )
 }
